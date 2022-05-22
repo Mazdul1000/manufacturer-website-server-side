b4d903f35453adfe46b6b91a7e6e112452ed4321
Mazdul1000 <mazdul1000@gmail.com>
2022-05-23 02:20:13 +0600

    added review api

--- index.js
+++ index.js
@@ -10,14 +10,100 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.118yr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Token verification:
+async function verifyToken(req, res, next){
+    const headerAuthor = req.headers.authorization;
+    if(!headerAuthor){
+        return res.status(401).send({message: 'Access Unauthorized'});
+    }
+
+    const token = headerAuthor.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(error, decoded){
+        if(error){
+            return res.status(403).send({message: 'Access forbidden'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+// MongoDB Config:
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.118yr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-    await client.connect();
+
+    try{
+     await client.connect();
+
+    const userCollection = client.db('agri-tools').collection('users');
+    const reviewCollection = client.db('agri-tools').collection('reviews');
+    const productCollection = client.db('agri-tools').collection('products');
+
+    // Admin verification:
+
+    const verifyAdmin = async(req, res, next) => {
+        const requestor = req.decoded.email;
+        const requestorAccount = await userCollection.findOne({email: requestor});
+
+        if(requestor.role === 'admin'){
+            next();
+        } 
+        else{
+            res.status(403).send({message: 'Access forbidden'})
+        }
+    }
+
 
     
+
+    // Creating users api
+    app.get('/users', verifyToken, async(req,res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+    })
+
+    // adding users to database:
+    app.put('/user/:email', async (req,res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = {email: email}
+        const options = {upsert: true};
+
+        const updateDoc = {
+            $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({email:email},
+            process.env.ACCESS_TOKEN, {expiresIn:'30d'})
+            res.send({result, token});
+    });
+
+    // Adding review api 
+    app.post('/review', async(req ,res) => {
+        const review = req.body;
+        const result = await reviewCollection.insertOne(review);
+        res.send(result)
+    })
+
+    //Creating Reviews Api 
+    app.get('/reviews', async (req, res) => {
+        const reviews = await reviewCollection.find().toArray();
+        res.send(reviews);
+    })
+
+    }
+
+    finally{
+
+    }
+
 }
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running the Agri-Tools server')
